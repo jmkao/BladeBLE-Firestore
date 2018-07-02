9f51faad9d5b6a82154b51c00133099fad5ccf24
James Kao <jmk17@cornell.edu>
2018-07-02 13:57:49 -0700

    Adding automatic update to firestire

--- import.js
+++ import.js
@@ -1,6 +1,6 @@
 const firestoreService = require('firestore-export-import');
 
-const serviceAccount = require("/home/jkao/Dropbox/projects/rfglow/firestore/bladeble-e714e-firebase-adminsdk-v93ee-43ec2a4dc3.json");
+const serviceAccount = require("./bladeble-e714e-firebase-adminsdk-v93ee-43ec2a4dc3.json");
 const databaseURL = "https://bladeble-e714e.firebaseio.com";
 
 var restoreFile = process.argv[2];
